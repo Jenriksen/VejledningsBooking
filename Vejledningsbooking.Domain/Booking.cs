fe607fddbedba26fe2d0aef4b29dc27420a40851
Jesper Henriksen <jesperhen89@gmail.com>
2020-04-03 09:11:15 +0200

    update

--- Vejledningsbooking.Domain/Booking.cs
+++ Vejledningsbooking.Domain/Booking.cs
@@ -6,9 +6,9 @@ namespace Vejledningsbooking.Domain
     public class Booking : Entity<BookingId>
     {
         public BookingId Id { get; private set; }
-        public TimeslotId timeslotId { get; private set; }
+        public TimeslotId TimeslotId { get; private set; }
 
-        public DateTime BookingStartDateTime { get; private set; }
+        public DateTime BookingStartDateTime { get; private set; } //refactor to value objects
         public DateTime BookingEndDateTime { get; private set; }
 
         public bool Expired { get; private set; } = false;
@@ -17,23 +17,23 @@ namespace Vejledningsbooking.Domain
             Apply(new Events.BookingOnTimeslotCreated
             {
                 BookingId = id,
-                TimeslotId = timeslotId,
+                TimeslotId = TimeslotId,
                 BookingStartDateTime = BookingStartDateTime,
                 BookingEndDateTime = BookingEndDateTime
             });
 
         public void UpdateBookingStartDateTime(BookingId id, DateTime datetime) =>
-            Apply(new Events.TimeslotStartDateUpdated
+            Apply(new Events.BookingStartTimeUpdated
             {
-                TimeslotId = id,
-                TimeslotStartDateTime = datetime
+                BookingId = id,
+                BookingStartDateTime = datetime
             });
 
         public void UpdateBookingEndDateTime(BookingId id, DateTime datetime) =>
-            Apply(new Events.TimeslotEndDateUpdated
+            Apply(new Events.BookingEndTimeUpdated
             {
-                TimeslotId = id,
-                TimeslotEndDateTime = datetime
+                BookingId = id,
+                BookingEndDateTime = datetime
             });
         protected override void EnsureValidState()
         {
@@ -42,7 +42,21 @@ namespace Vejledningsbooking.Domain
 
         protected override void When(object @event)
         {
-            throw new NotImplementedException();
+            switch (@event)
+            {
+                case Events.BookingOnTimeslotCreated e:
+                    Id = new BookingId(e.BookingId);
+                    TimeslotId = new TimeslotId(e.TimeslotId);
+                    BookingStartDateTime = e.BookingStartDateTime;
+                    BookingEndDateTime = e.BookingEndDateTime;
+                    break;
+                case Events.BookingStartTimeUpdated e:
+                    BookingStartDateTime = e.BookingStartDateTime;
+                    break;
+                case Events.BookingEndTimeUpdated e:
+                    BookingEndDateTime = e.BookingEndDateTime;
+                    break;
+            }
         }
     }
 }
